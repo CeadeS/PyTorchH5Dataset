b26fce4bf85fb1b1415e78a0ae481c5d818854ce
martinh <Martin.Hofmann@tu-ilmenau.de>
2022-01-28 17:29:20 +0100

    fix benchmarking notebook

--- scripts/run_benchmark.py
+++ scripts/run_benchmark.py
@@ -11,26 +11,12 @@ if __name__ == '__main__':
 
 
     print('H5')
-    for num_workers in [0,0]:
+    for num_workers in [0,0,1,2,3,4,5,6,7,8]:
         benchmarkdataset = BenchmarkDataset()
         batch_size = 100
         dataLoader = H5DataLoader(dataset=benchmarkdataset.h5dataset, device='cpu:0', batch_size=batch_size,
                                             num_workers=num_workers, return_meta_indices=True, pin_memory=True)
 
-        l = 0
-        to = time()
-        for idx, (sample, (meta_class, sample_indices)) in enumerate(dataLoader):
-            l+=sample.sum()
-        print(f"num workers: {num_workers:3d}  time elapsed: {time() - to:3.2f} sample sum {l}")
-
-    print('H5 my collate')
-    for num_workers in [0,1,2,3,4,5,6,7,8]:
-        benchmarkdataset = BenchmarkDataset()
-
-        batch_size = 100
-        dataLoader = H5DataLoader(dataset=benchmarkdataset.h5dataset, device='cpu:0', batch_size=batch_size,
-                                            num_workers=num_workers, return_meta_indices=True, pin_memory=True
-                                            )
         l = 0
         to = time()
         for idx, (sample, (meta_class, sample_indices)) in enumerate(dataLoader):
