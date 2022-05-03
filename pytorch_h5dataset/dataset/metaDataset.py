fdc8a7236d20a260508edb47997398b2929a0a34
martinh <Martin.Hofmann@tu-ilmenau.de>
2022-05-03 12:39:32 +0200

    repair indexing

--- pytorch_h5dataset/dataset/metaDataset.py
+++ pytorch_h5dataset/dataset/metaDataset.py
@@ -58,7 +58,9 @@ class H5MetaDataset(Dataset, ABC):
                 if i%max_n_group==0:
                     group_key = str(int(i//max_n_group))
                     hdf5_file.create_group(group_key)
+                print(str(int(i%max_n_group)))
                 hdf5_file[group_key].create_dataset(f'samples/{str(int(i%max_n_group))}', data=d)
+                #h5_file[group_key].create_dataset(f"samples/{int(idx%max_n_group)}", data=batch)
                 print(f"\r{int(i):7d} of {len(tar_file_contents_names):7d} written", end='')
                 if i % 1000 == 0:
                     logging.info(f"{int(i):7d} of {len(tar_file_contents_names):7d} written")
